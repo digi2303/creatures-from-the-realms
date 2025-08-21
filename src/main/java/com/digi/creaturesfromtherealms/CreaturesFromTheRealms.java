7a36756f199f83e47aa9a55e4d3be89937608cc9
Digi2303 <egerobrox3@gmail.com>
2025-08-21 18:51:10 +0300

    Added Snow Item Group

--- src/main/java/com/digi/creaturesfromtherealms/CreaturesFromTheRealms.java
+++ src/main/java/com/digi/creaturesfromtherealms/CreaturesFromTheRealms.java
@@ -1,6 +1,7 @@
 package com.digi.creaturesfromtherealms;
 
 import com.digi.creaturesfromtherealms.block.ModBlocks;
+import com.digi.creaturesfromtherealms.item.ModItemGroups;
 import com.digi.creaturesfromtherealms.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,7 @@ public class CreaturesFromTheRealms implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
 	}
---
+++ src/main/java/com/digi/creaturesfromtherealms/item/ModItemGroups.java
@@ -0,0 +1,28 @@
+package com.digi.creaturesfromtherealms.item;
+
+import com.digi.creaturesfromtherealms.CreaturesFromTheRealms;
+import com.digi.creaturesfromtherealms.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup SNOW_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(CreaturesFromTheRealms.MOD_ID, "snow_group"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.SNOW_GIRAFFE_FUR))
+                    .displayName(Text.translatable("itemgroup.creatures-from-the-realms.snow_group"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.SNOW_GIRAFFE_FUR);
+                        entries.add(ModBlocks.SNOW_GIRAFFE_FUR_BLOCK);
+                    }).build());
+
+
+
+    public static void registerItemGroups() {
+        CreaturesFromTheRealms.LOGGER.info("Registering Item Groups for " + CreaturesFromTheRealms.MOD_ID);
+    }
+}
